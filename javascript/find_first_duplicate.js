443d525044e29a41a18a75a7556f196838317290
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-05-17 11:05:42 -0400

    test passing

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,9 +1,20 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  let duplicate = []
+  for (let i = 0; i < arr.length; i ++) {
+    if(duplicate.includes(arr[i])){
+      return arr[i]
+    } else {
+      duplicate.push(arr[i])
+    }
+  }
+  return -1;
 }
 
 if (require.main === module) {
-  // add your own tests in here
+
+  console.log(findFirstDuplicate([1,2,3,4,3]))
+  console.log(findFirstDuplicate([5,6,7,8]))
+
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
